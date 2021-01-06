ba52bb89bb751e0174b6437c8bbd1961d2c905d6
Milo123459 <milo5huntington@gmail.com>
2021-01-06 14:42:35 +0000

    add donate button

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,15 @@
       >
       <a href="https://discord.com/api/oauth2/authorize?client_id=765156777607823380&permissions=8&scope=bot%20applications.commands" class="transition duration-500 text-purple-400 hover:text-white animate-bounce pr-7">
       Invite</a>
+      <a href="https://top.gg/bot/765156777607823380/vote" class="transition duration-500 text-purple-400 hover:text-white animate-bounce pr-7">Vote</a>
     </div>
     <router-view />
   </body>
+  <section class="text-center bg-gray-700">
+      <a href="https://donatebot.io/checkout/784470505607528448" class="transition duration-500 ease-in-out bg-indigo-500 hover:bg-indigo-600 transform hover:-translate-y-1 hover:scale-110 rounded-md tracking-widest text-black text-xl font-medium title-font">
+        <code>Donate</code>
+</a>
+<br><br><br>
+
+  </section>
 </template>
