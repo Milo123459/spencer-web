e4c4d161670e67d1009d25d6202e03d03c43807c
Milo123459 <milo5huntington@gmail.com>
2021-01-06 08:45:43 +0000

    fix

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -85,27 +85,6 @@
             </p>
           </div>
         </div>
-        <div class="xl:w-1/4 md:w-1/2 p-4">
-          <div class="bg-gray-100 p-6 rounded-lg">
-            <img
-              class="h-40 rounded w-full object-cover object-center mb-6"
-              src="https://dummyimage.com/723x403"
-              alt="content"
-            />
-            <h3
-              class="tracking-widest text-indigo-500 text-xs font-medium title-font"
-            >
-              SUBTITLE
-            </h3>
-            <h2 class="text-lg text-gray-900 font-medium title-font mb-4">
-              San Francisco
-            </h2>
-            <p class="leading-relaxed text-base">
-              Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-              hexagon disrupt edison bulbche.
-            </p>
-          </div>
-        </div>
       </div>
     </div>
   </section>
