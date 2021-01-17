050387a8964ad499cc1304989280bda460128792
Milo123459 <milo5huntington@gmail.com>
2021-01-17 10:21:40 +0000

    remove comment

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,6 @@
 
   <div class="hidden sm:hidden">
     <div class="px-2 pt-2 pb-3 space-y-1">
-      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
       <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
       <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
       <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Invite</a>
