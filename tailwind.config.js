e0340a8da33912d00fec02727400dd0009060982
Milo123459 <milo5huntington@gmail.com>
2021-01-19 16:45:05 +0000

    work please

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-        colors: {
-            blurple: {
-                DEFAULT: '#7289DA'
-            }
-        }
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+          colors: {
+              blurple: {
+                  DEFAULT: '#7289DA'
+              }
+          }
+      },
     },
-  },
-  variants: {
-    extend: {
-      backgroundColor: ["active"],
+    variants: {
+      extend: {
+        backgroundColor: ["active"],
+      },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+  };
+  
